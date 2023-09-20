82d5656d3d564a02632eda06d7ff44b611c79b62
Anthony Bibbs <abibbs@users.noreply.github.com>
2023-09-20 13:24:31 -0700

    Header update (#5)
    * Updated header to replace text with logo
    
    * Replaced favicon with FriendlyChat logo

--- friendlychat-frontend/src/components/ChatBar.js
+++ friendlychat-frontend/src/components/ChatBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../img/friendlychat-logo.png';
 
 const ChatBar = () => {
   return (
     <div className="chat__sidebar">
-      <h2>FriendlyChat</h2>
-
+      <div>
+        <img src={logo} alt="FriendlyChat Logo" className='logo-img'/>
+      </div>
       <div>
         <h4 className="chat__header">ACTIVE USERS</h4>
         <div className="chat__users">
